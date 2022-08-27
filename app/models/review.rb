003f6cc72f07868b0659ece665d9380fd9e61399
Judy Mwai <juddiemukami@gmail.com>
2022-08-27 16:22:31 +0300

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+
+    # end
+
+    # use the belongs_to macro instead of our custom method:
+    belongs_to :game
   
 end
